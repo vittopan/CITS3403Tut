639f0901c2dc4689c128ba94966ddc058c4665f7
vittopan <vittopan19@gmail.com>
2024-05-24 12:27:25 +0800

    login page and tesing page created with added info.

--- app.py
+++ app.py
@@ -45,6 +45,13 @@ def flaskInfo():
 def labs():
     return render_template('labs.html')
 
+@app.route('/testing')
+def testing():
+    return render_template('testing.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
